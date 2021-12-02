760f9371149e447a01692d1403a121543d21923e
Boris Batkin <bbatkin@gmail.com>
2021-12-02 10:49:15 -0800

    compression profiler

--- test/main.c
+++ test/main.c
@@ -54,11 +54,16 @@ int main(int argc, char** argv) {
     }
     printf("noi_compress %i x %i\n", w, h);
     int csize;
+    clock_t t0 = clock();
     void * cbytes = noi_compress(pixels, w, h, &csize);
+    clock_t t1 = clock();
     if ( !cbytes ) {
       printf("compression failed\n");
       return -4;
     }
+    double sec = ((double)(t1-t0))/CLOCKS_PER_SEC;
+    double mb = ((double)(w*h*3))/1024./1024.;
+    printf("%i mb in %.2f sec, %.1fmb/sec\n", ((int)mb), sec, mb/sec );
     if ( strcmp(argv[1],"-pc")==0 ) {
       uint8_t * cpixels = noi_decompress(cbytes, NULL, NULL, NULL);
       psnr(pixels, cpixels, w*h);
