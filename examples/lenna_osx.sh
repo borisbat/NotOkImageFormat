23e20dc41f7601a32d7b756f5a02daf3e2953522
Boris Batkin <bbatkin@gmail.com>
2021-12-04 01:00:09 -0800

    also 2-1-1 profile

--- examples/lenna_osx.sh
+++ examples/lenna_osx.sh
@@ -6,6 +6,10 @@ open lenna_test_16_1_1.png
 ../bin/noi -pd lenna_4_1_1.noi lenna_test_4_1_1.png
 open lenna_test_4_1_1.png
 
+../bin/noi -pc lenna.png lenna_2_1_1.noi 2_1_1
+../bin/noi -pd lenna_2_1_1.noi lenna_test_2_1_1.png
+open lenna_test_2_1_1.png
+
 ../bin/noi -pc lenna.png lenna_1_1_1.noi 1_1_1
 ../bin/noi -pd lenna_1_1_1.noi lenna_test_1_1_1.png
 open lenna_test_1_1_1.png
